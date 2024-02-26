f6455a9bc3d1baedfe6298138e7638769e54d9a3
kadote870 <kadote870@gmail.com>
2024-02-26 13:07:37 +0100

    lekcja 23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import {AppComponent} from './app.component';
 import {ServerComponent} from "./server/server.component";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ServerComponent
+        ServerComponent,
+        ServersComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `
+    <h4>elo melo</h4>
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
