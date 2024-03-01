326162430e8f7037f7abfea6565b851861928291
kadote870 <kadote870@gmail.com>
2024-03-01 17:04:01 +0100

    lekcja 24

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
   template: `
-    <h4>elo melo</h4>
+    <h2>elo melo</h2>
     <app-server></app-server>
     <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
