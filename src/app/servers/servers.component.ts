24df0b02a61898c9da96c46981dbddd77c9eec15
kadote870 <kadote870@gmail.com>
2024-03-03 14:51:47 +0100

    lekcja 28

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
-  // selector: '.app-servers',
-  template: `
-    <h2>elo melo</h2>
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+    selector: '[app-servers]',
+    // selector: '.app-servers',
+    // template: `
+    //   <h2>elo melo</h2>
+    //   <app-server></app-server>
+    //   <app-server></app-server>`,
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+    allowNewServer = false;
 
+    constructor() {
+        setTimeout(()=>{
+            this.allowNewServer=true},2000
+        )
+    }
+
+    ngOnInit() {
+    }
 }
